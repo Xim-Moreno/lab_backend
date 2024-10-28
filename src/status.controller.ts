d08df61eefd5e404b9acc3de2970d5a056c1ab54
Moreno Paz Ximena (BGSW/EBS3.1-SX) <Ximena.MorenoPaz@mx.bosch.com>
2024-10-27 23:31:15 -0600

    status controller

--- src/status.controller.ts
+++ src/status.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 
-@Controller('')  // Ruta /status
+@Controller('status')  // Ruta /status
 export class StatusController {
   @Get()
   getStatus() {
